c466f1979830819d288902aa7ffdb490afaff78e
Wock10 <timothy.joy@joypalaceresale.com>
2023-02-11 11:22:26 -0500

    beginning of target function

--- src/bot.ts
+++ src/bot.ts
@@ -3,6 +3,7 @@ import * as dotenv from 'dotenv'
 import path from 'path'
 import interactionCreate from './hooks/interactionCreate'
 import ready from './hooks/ready'
+import { User } from './classes/user'
 
 dotenv.config({ path: path.resolve('./config.env') })
 const token = process.env.DSCRD_BOT_TK
@@ -18,11 +19,26 @@ interactionCreate(client)
 
 client.login(token)
 client.on('messageCreate', message => {
+  if (message.author.id === client.user?.id) return
   if (message.channelId === process.env.GAME_CHANNEL) {
-    exampleMethod(message)
+    target(message)
   }
 })
 
-function exampleMethod (message : Message) {
+async function target (message: Message) {
+  const text = message.content.toLowerCase().replace(/\s\s+/g, ' ').split(' ')
+  const command = text[0]
+  if (command === '!target') {
+    const user = await User.getByDiscordId(message.author.id)
+    if (user) {
+      message.channel.send('Test worked!')
+      // continue
+    } else {
+      // redirect to make an account
+    }
+    // check if user has created an account
+  } else {
+    // thumbs down
+  }
   console.log(message.content)
 }
